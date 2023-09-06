412bb84e1a878db6af6f6c0995a715283c3cb16d
Andrij-Panasenko <andrii.panasenko295@gmail.com>
2023-09-06 19:44:27 +0300

    Task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+btnStart.addEventListener('click', onStartClick);
+btnStop.addEventListener('click', onStopClick);
+
+let intervalId = null;
+btnStop.disabled = true;
+
+function onStartClick() { 
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+
+  intervalId = setInterval(() => { 
+    document.body.style.backgroundColor = getRandomHexColor();
+    isActive = true;
+  }, 1000)
+};
+
+
+
+function onStopClick() {
+  clearInterval(intervalId);
+  intervalId = null;
+  btnStop.disabled = true;
+  btnStart.disabled = false;
+ };
